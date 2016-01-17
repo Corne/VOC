7d09cff5c378c8228f2a5bdd849a9ddbe0c0549a
Corne van Beek <cornevbeek123@live.nl>
2016-01-17 21:06:12 +0100

    added bank interface

---
+++ test/VOC.Core.Test/Trading/BankTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VOC.Core.Test.Trading
+{
+    class BankTest
+    {
+    }
+}
