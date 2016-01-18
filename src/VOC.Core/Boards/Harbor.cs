7d81a485f1b767f723a73e7dbeebbf8a37056287
Corne <cornevbeek123@live.nl>
2016-01-18 14:12:53 +0100

    added harbor implementation

--- src/VOC.Core/Boards/Harbor.cs
+++ src/VOC.Core/Boards/Harbor.cs
@@ -12,6 +12,17 @@ namespace VOC.Core.Boards
 
         public Harbor(MaterialType material, IEdge edge, ITile tile)
         {
+            if (edge == null)
+                throw new ArgumentNullException(nameof(edge));
+            if (tile == null)
+                throw new ArgumentNullException(nameof(tile));
+            if (!Enum.IsDefined(typeof(MaterialType), material) || material == MaterialType.Sea)
+                throw new ArgumentException("Invalid discount material, should be defined enum value and not sea type");
+            if (tile.Rawmaterial != MaterialType.Sea)
+                throw new ArgumentException("Tile should be sea tile");
+            if (!edge.IsAdjacentTo(tile))
+                throw new ArgumentException("Edge and tile should be adjacent");
+
             Discount = material;
             Edge = edge;
             Tile = tile;
