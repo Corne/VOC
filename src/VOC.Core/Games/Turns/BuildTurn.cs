432e01057c3cfbf3fa91c12eb76e5abbcc95c0ee
Corne van Beek <cornevbeek123@live.nl>
2016-02-23 20:17:13 +0100

    implemented build turn

---
+++ src/VOC.Core/Games/Turns/BuildTurn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using VOC.Core.Players;
+
+namespace VOC.Core.Games.Turns
+{
+    public class BuildTurn : IBuildTurn
+    {
+        private object buildLock = new object();
+        private GameCommand expectedCommand = GameCommand.BuildEstablisment;
+        public BuildTurn(IPlayer player)
+        {
+            if (player == null)
+                throw new ArgumentNullException(nameof(player));
+
+            Player = player;
+        }
+
+        public IPlayer Player { get; }
+        public event EventHandler Ended;
+
+        public void AfterExecute(GameCommand command)
+        {
+            lock (buildLock)
+            {
+                if (!CanExecute(command)) return;
+
+                switch (command)
+                {
+                    case GameCommand.BuildEstablisment:
+                        expectedCommand = GameCommand.BuildRoad;
+                        break;
+                    case GameCommand.BuildRoad:
+                        expectedCommand = GameCommand.NextState;
+                        Ended?.Invoke(this, EventArgs.Empty);
+                        break;
+                }
+            }
+        }
+
+        public bool CanExecute(GameCommand command)
+        {
+            return command == expectedCommand;
+        }
+    }
+}
---
+++ test/VOC.Core.Test/Games/Turns/BuildTurnTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Moq;
+using VOC.Core.Games.Turns;
+using VOC.Core.Players;
+using Xunit;
+
+namespace VOC.Core.Test.Games.Turns
+{
+    public class BuildTurnTest
+    {
+        [Fact]
+        public void BuildTurnCantBeConstructedWithoutPlayer()
+        {
+            Assert.Throws<ArgumentNullException>(() => new BuildTurn(null));
+        }
+
+        [Fact]
+        public void BuildTurnConstructionSucceeds()
+        {
+            var player = new Mock<IPlayer>();
+            var turn = new BuildTurn(player.Object);
+            Assert.Equal(player.Object, turn.Player);
+        }
+
+        [Fact]
+        public void ExpectBuildEstablismentToBeExecutable()
+        {
+            var player = new Mock<IPlayer>();
+            var turn = new BuildTurn(player.Object);
+
+            Assert.True(turn.CanExecute(GameCommand.BuildEstablisment));
+            Assert.False(turn.CanExecute(GameCommand.BuildRoad));
+        }
+
+        [Fact]
+        public void ExpectBuildRoadTeBeExecutableAfterEstablimentBuild()
+        {
+            var player = new Mock<IPlayer>();
+            var turn = new BuildTurn(player.Object);
+
+            turn.AfterExecute(GameCommand.BuildEstablisment);
+
+            Assert.False(turn.CanExecute(GameCommand.BuildEstablisment));
+            Assert.True(turn.CanExecute(GameCommand.BuildRoad));
+        }
+
+        [Fact]
+        public void ExpectBuildTurnToEndAfterRoadAndEstablismentBuildExecuted()
+        {
+            var player = new Mock<IPlayer>();
+            var turn = new BuildTurn(player.Object);
+
+            bool ended = false;
+            turn.Ended += (sender, args) => { ended = true; };
+
+            turn.AfterExecute(GameCommand.BuildEstablisment);
+            turn.AfterExecute(GameCommand.BuildRoad);
+
+            Assert.True(ended);
+            Assert.False(turn.CanExecute(GameCommand.BuildEstablisment));
+            Assert.False(turn.CanExecute(GameCommand.BuildRoad));
+        }
+    }
+}
