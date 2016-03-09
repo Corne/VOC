e2a1fe3cfbc41e36d0970395a4cdcbe5b09616d8
Corne van Beek <cornevbeek123@live.nl>
2016-03-09 20:14:25 +0100

    implemented moverobber command

---
+++ src/VOC.Core/Games/Commands/MoveRobberCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOC.Core.Boards;
+using VOC.Core.Games.Turns;
+using VOC.Core.Items;
+using VOC.Core.Players;
+
+namespace VOC.Core.Games.Commands
+{
+    public class MoveRobberCommand : IPlayerCommand
+    {
+        private readonly IRobber robber;
+        private readonly ITile tile;
+        public MoveRobberCommand(IPlayer player, IRobber robber, ITile tile)
+        {
+            if (player == null)
+                throw new ArgumentNullException(nameof(player));
+            if (robber == null)
+                throw new ArgumentNullException(nameof(robber));
+            if (tile == null)
+                throw new ArgumentNullException(nameof(tile));
+            Player = player;
+            this.robber = robber;
+            this.tile = tile;
+        }
+
+        public IPlayer Player { get; }
+
+        public GameCommand Type { get { return GameCommand.MoveRobber; } }
+
+        public void Execute()
+        {
+            //CvB Todo: This seems kinda weird to have a whole class for this?
+            robber.Move(tile);
+        }
+    }
+}
---
+++ test/VOC.Core.Test/Games/Commands/MoveRobberCommandTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using VOC.Core.Boards;
+using VOC.Core.Games.Commands;
+using VOC.Core.Items;
+using VOC.Core.Players;
+using Xunit;
+
+namespace VOC.Core.Test.Games.Commands
+{
+    public class MoveRobberCommandTest
+    {
+        [Fact]
+        public void CommandCantBeCreatedWithoutPlayer()
+        {
+            var robber = new Mock<IRobber>();
+            var tile = new Mock<ITile>();
+            Assert.Throws<ArgumentNullException>(() => new MoveRobberCommand(null, robber.Object, tile.Object));
+        }
+
+        [Fact]
+        public void CommandCantBeCreatedWithoutRobber()
+        {
+            var player = new Mock<IPlayer>();
+            var tile = new Mock<ITile>();
+            Assert.Throws<ArgumentNullException>(() => new MoveRobberCommand(player.Object, null, tile.Object));
+
+        }
+
+        [Fact]
+        public void CommandCantBeCreatedWithoutTile()
+        {
+            var player = new Mock<IPlayer>();
+            var robber = new Mock<IRobber>();
+            Assert.Throws<ArgumentNullException>(() => new MoveRobberCommand(player.Object, robber.Object, null));
+        }
+
+        [Fact]
+        public void ExecuteTest()
+        {
+            var player = new Mock<IPlayer>();
+            var robber = new Mock<IRobber>();
+            var tile = new Mock<ITile>();
+
+            var command = new MoveRobberCommand(player.Object, robber.Object, tile.Object);
+            command.Execute();
+
+            robber.Verify(r => r.Move(tile.Object));
+        }
+    }
+}
