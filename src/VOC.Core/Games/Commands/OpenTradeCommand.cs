d6badf36426346d3063e6455178246bd94cfb748
Corne van Beek <cornevbeek123@live.nl>
2016-03-12 14:27:03 +0100

    implemenetd opentrade command

---
+++ src/VOC.Core/Games/Commands/OpenTradeCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOC.Core.Games.Turns;
+using VOC.Core.Players;
+using VOC.Core.Trading;
+
+namespace VOC.Core.Games.Commands
+{
+    public class OpenTradeCommand : IPlayerCommand
+    {
+        private readonly IMarket market;
+        private readonly ITrade trade;
+
+        public OpenTradeCommand(IPlayer player, IMarket market, ITrade trade)
+        {
+            if (player == null)
+                throw new ArgumentNullException(nameof(player));
+            if (market == null)
+                throw new ArgumentNullException(nameof(market));
+            if (trade == null)
+                throw new ArgumentNullException(nameof(trade));
+            Player = player;
+            this.market = market;
+            this.trade = trade;
+        }
+
+        public IPlayer Player { get; }
+
+        public GameCommand Type { get { return GameCommand.Trade; } }
+
+        public void Execute()
+        {
+            //CvB Todo: This seems kinda weird to have a whole class for this?
+            market.OpenTrade(trade);
+        }
+    }
+}
---
+++ test/VOC.Core.Test/Games/Commands/OpenTradeCommandTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using VOC.Core.Games.Commands;
+using VOC.Core.Players;
+using VOC.Core.Trading;
+using Xunit;
+
+namespace VOC.Core.Test.Games.Commands
+{
+    public class OpenTradeCommandTest
+    {
+        public static IEnumerable<object> InvalidConstruction
+        {
+            get
+            {
+                yield return new object[] { null, new Mock<IMarket>().Object, new Mock<ITrade>().Object };
+                yield return new object[] { new Mock<IPlayer>().Object, null, new Mock<ITrade>().Object };
+                yield return new object[] { new Mock<IPlayer>().Object, new Mock<IMarket>().Object, null };
+            }
+        }
+
+        [Theory, MemberData(nameof(InvalidConstruction))]
+        public void ExpectConstructionIfFailOnNullParameter(IPlayer player, IMarket market, ITrade trade)
+        {
+            Assert.Throws<ArgumentNullException>(() => new OpenTradeCommand(player, market, trade));
+        }
+
+        [Fact]
+        public void ExecuteOpensTradeOnMarkter()
+        {
+            var player = new Mock<IPlayer>();
+            var market = new Mock<IMarket>();
+            var trade = new Mock<ITrade>();
+
+            var command = new OpenTradeCommand(player.Object, market.Object, trade.Object);
+            command.Execute();
+
+            market.Verify(m => m.OpenTrade(trade.Object));
+        }
+    }
+}
