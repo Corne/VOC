3b3f583a5e7891c548cde7166723be075fd3f998
Corne van Beek <cornevbeek123@live.nl>
2016-03-12 15:30:31 +0100

    implemented canceltradecommand

---
+++ test/VOC.Core.Test/Games/Commands/CancelTradeCommandTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using VOC.Core.Games.Commands;
+using VOC.Core.Players;
+using VOC.Core.Trading;
+using Xunit;
+
+namespace VOC.Core.Test.Games.Commands
+{
+    public class CancelTradeCommandTest
+    {
+        public static IEnumerable<object> InvalidConstruction
+        {
+            get
+            {
+                yield return new object[] { null, new Mock<ITrade>().Object };
+                yield return new object[] { new Mock<IPlayer>().Object, null };
+            }
+        }
+
+        [Theory, MemberData(nameof(InvalidConstruction))]
+        public void ConstructionFailsWithNullParameter(IPlayer player, ITrade trade)
+        {
+            Assert.Throws<ArgumentNullException>(() => new CancelTradeCommand(player, trade));
+        }
+
+        [Fact]
+        public void ExecuteCancelsTrade()
+        {
+            var player = new Mock<IPlayer>();
+            var trade = new Mock<ITrade>();
+            var command = new CancelTradeCommand(player.Object, trade.Object);
+            command.Execute();
+
+            trade.Verify(t => t.Cancel());
+        }
+    }
+}
