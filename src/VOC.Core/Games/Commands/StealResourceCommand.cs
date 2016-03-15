3865ad5a1e87720074b8c5db49efb09afba654fd
Corne van Beek <cornevbeek123@live.nl>
2016-03-15 19:48:35 +0100

    implemented StealResourcesCommand

---
+++ src/VOC.Core/Games/Commands/StealResourceCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOC.Core.Games.Turns;
+using VOC.Core.Items.RawMaterials;
+using VOC.Core.Players;
+
+namespace VOC.Core.Games.Commands
+{
+    public class StealResourceCommand : IPlayerCommand
+    {
+        private static readonly Random random = new Random();
+        private readonly IPlayer victim;
+
+        public StealResourceCommand(IPlayer player, IPlayer victim)
+        {
+            if (player == null)
+                throw new ArgumentNullException(nameof(player));
+            if (victim == null)
+                throw new ArgumentNullException(nameof(victim));
+            if (victim.Inventory.Count() == 0)
+                throw new InvalidOperationException("Can't steal from a player without resources");
+
+            Player = player;
+            this.victim = victim;
+        }
+
+        public IPlayer Player { get; }
+
+        public GameCommand Type { get { return GameCommand.StealResource; } }
+
+        public void Execute()
+        {
+            IRawMaterial[] inventory = victim.Inventory.ToArray();
+            MaterialType type = inventory[random.Next(inventory.Length)].Type;
+            var resource = victim.TakeResources(type);
+            Player.AddResources(resource);
+        }
+    }
+}
---
+++ test/VOC.Core.Test/Games/Commands/StealResourceCommandTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using VOC.Core.Games.Commands;
+using VOC.Core.Items.RawMaterials;
+using VOC.Core.Players;
+using Xunit;
+
+namespace VOC.Core.Test.Games.Commands
+{
+    public class StealResourceCommandTest
+    {
+        public static IEnumerable<object> NullConstruction
+        {
+            get
+            {
+                yield return new object[] { null, new Mock<IPlayer>().Object };
+                yield return new object[] { new Mock<IPlayer>().Object, null };
+            }
+        }
+
+        [Theory, MemberData(nameof(NullConstruction))]
+        public void CantConstructWithNull(IPlayer player, IPlayer victim)
+        {
+            Assert.Throws<ArgumentNullException>(() => new StealResourceCommand(player, victim));
+        }
+
+        [Fact]
+        public void ConstructionFailsIfVictimNoResources()
+        {
+            var player = new Mock<IPlayer>();
+            var victim = new Mock<IPlayer>();
+            victim.Setup(v => v.Inventory).Returns(new IRawMaterial[0]);
+            Assert.Throws<InvalidOperationException>(() => new StealResourceCommand(player.Object, victim.Object));
+        }
+
+        private IRawMaterial CreateMaterial(MaterialType type)
+        {
+            var mock = new Mock<IRawMaterial>();
+            mock.Setup(m => m.Type).Returns(type);
+            return mock.Object;
+        }
+
+        [Fact]
+        public void ExecuteTakesResourceFromVictimAndAddToPlayer()
+        {
+            var player = new Mock<IPlayer>();
+            var victim = new Mock<IPlayer>();
+            victim.Setup(v => v.Inventory).Returns(
+                new IRawMaterial[2] {
+                    CreateMaterial(MaterialType.Brick),
+                    CreateMaterial(MaterialType.Lumber)
+                });
+            victim.Setup(v => v.TakeResources(It.IsAny<MaterialType[]>()))
+                .Returns<MaterialType[]>(m => victim.Object.Inventory.Where(i => m.Contains(i.Type)).ToArray());
+
+            var command = new StealResourceCommand(player.Object, victim.Object);
+            command.Execute();
+
+            victim.Verify(v => v.TakeResources(It.Is<MaterialType[]>(m => m.Length == 1)));
+            player.Verify(v => v.AddResources(It.Is<IRawMaterial[]>(m => m.Length == 1)));
+        }
+    }
+}
