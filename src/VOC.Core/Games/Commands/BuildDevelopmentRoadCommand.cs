daff10824609b239a5be85eebf97deb6e68060cb
Corne van Beek <cornevbeek123@live.nl>
2016-03-16 20:43:47 +0100

    implemented builddevelopmentroadcommand

---
+++ src/VOC.Core/Games/Commands/BuildDevelopmentRoadCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOC.Core.Boards;
+using VOC.Core.Games.Turns;
+using VOC.Core.Players;
+
+namespace VOC.Core.Games.Commands
+{
+    public class BuildDevelopmentRoadCommand : IPlayerCommand
+    {
+        private readonly IBoard board;
+        private readonly IEdge edge;
+
+        public BuildDevelopmentRoadCommand(IPlayer player, IBoard board, IEdge edge)
+        {
+            if (player == null)
+                throw new ArgumentNullException(nameof(player));
+            if (board == null)
+                throw new ArgumentNullException(nameof(board));
+            if (edge == null)
+                throw new ArgumentNullException(nameof(edge));
+
+            Player = player;
+            this.board = board;
+            this.edge = edge;
+        }
+
+        public IPlayer Player { get; }
+
+        public GameCommand Type { get { return GameCommand.BuildRoad; } }
+
+        public void Execute()
+        {
+            board.BuildRoad(edge, Player);
+        }
+    }
+}
---
+++ test/VOC.Core.Test/Games/Commands/BuildDevelopmentRoadCommandTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using VOC.Core.Boards;
+using VOC.Core.Games.Commands;
+using VOC.Core.Items.RawMaterials;
+using VOC.Core.Players;
+using Xunit;
+
+namespace VOC.Core.Test.Games.Commands
+{
+    public class BuildDevelopmentRoadCommandTest
+    {
+        public static IEnumerable<object> NullConstruction
+        {
+            get
+            {
+                yield return new object[] { null, new Mock<IBoard>().Object, new Mock<IEdge>().Object };
+                yield return new object[] { new Mock<IPlayer>().Object, null, new Mock<IEdge>().Object };
+                yield return new object[] { new Mock<IPlayer>().Object, new Mock<IBoard>().Object, null };
+            }
+        }
+
+        [Theory, MemberData(nameof(NullConstruction))]
+        public void CantConstructWithNullParameter(IPlayer player, IBoard board, IEdge edge)
+        {
+            Assert.Throws<ArgumentNullException>(() => new BuildDevelopmentRoadCommand(player, board, edge));
+        }
+
+        [Fact]
+        public void ExecuteTest()
+        {
+            var player = new Mock<IPlayer>();
+            var board = new Mock<IBoard>();
+            var edge = new Mock<IEdge>();
+
+            var command = new BuildDevelopmentRoadCommand(player.Object, board.Object, edge.Object);
+            command.Execute();
+
+            board.Verify(b => b.BuildRoad(edge.Object, player.Object));
+            player.Verify(p => p.TakeResources(It.IsAny<MaterialType[]>()), Times.Never);
+        }
+    }
+}
