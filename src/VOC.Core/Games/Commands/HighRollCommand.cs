e18e47b8033f28c5d723c6a774b9b6b62ff862f4
Corne van Beek <cornevbeek123@live.nl>
2016-03-20 09:21:17 +0100

    implemented highrollcommand

---
+++ src/VOC.Core/Games/Commands/HighRollCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOC.Core.Games.Turns;
+using VOC.Core.Items;
+using VOC.Core.Players;
+
+namespace VOC.Core.Games.Commands
+{
+    public class HighRollCommand : IPlayerCommand
+    {
+        private readonly IDice dice;
+        public HighRollCommand(IPlayer player, IDice dice)
+        {
+            if (player == null)
+                throw new ArgumentNullException(nameof(player));
+            if (dice == null)
+                throw new ArgumentNullException(nameof(dice));
+
+            Player = player;
+            this.dice = dice;
+        }
+
+        public IPlayer Player { get; }
+
+        public GameCommand Type { get { return GameCommand.RollDice; } }
+
+        public void Execute()
+        {
+            dice.Roll();
+        }
+    }
+}
---
+++ test/VOC.Core.Test/Games/Commands/HighRollCommandTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using VOC.Core.Games.Commands;
+using VOC.Core.Items;
+using VOC.Core.Players;
+using Xunit;
+
+namespace VOC.Core.Test.Games.Commands
+{
+    public class HighRollCommandTest
+    {
+        public static IEnumerable<object> NullConstruction
+        {
+            get
+            {
+                yield return new object[] { null, new Mock<IDice>().Object };
+                yield return new object[] { new Mock<IPlayer>().Object, null };
+            }
+        }
+        
+        [Theory, MemberData(nameof(NullConstruction))]
+        public void CantConstructWithNull(IPlayer player, IDice dice)
+        {
+            Assert.Throws<ArgumentNullException>(() => new HighRollCommand(player, dice));
+        }
+
+        [Fact]
+        public void ExecuteRollsDice()
+        {
+            var player = new Mock<IPlayer>();
+            var dice = new Mock<IDice>();
+            var command = new HighRollCommand(player.Object, dice.Object);
+
+            command.Execute();
+            dice.Verify(d => d.Roll());
+        }
+    }
+}
