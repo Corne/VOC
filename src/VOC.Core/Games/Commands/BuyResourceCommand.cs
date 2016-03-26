1c35b635e402a3b3775a12e97b31fc5ea148b8bf
Corne van Beek <cornevbeek123@live.nl>
2016-03-26 12:44:32 +0100

    added BuyResourceCommand

---
+++ src/VOC.Core/Games/Commands/BuyResourceCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOC.Core.Games.Turns;
+using VOC.Core.Items.RawMaterials;
+using VOC.Core.Players;
+using VOC.Core.Trading;
+
+namespace VOC.Core.Games.Commands
+{
+    public class BuyResourceCommand : IPlayerCommand
+    {
+        private readonly IBank bank;
+        private readonly MaterialType buy;
+        private readonly MaterialType offer;
+
+        public BuyResourceCommand(IPlayer player, IBank bank, MaterialType buy, MaterialType offer)
+        {
+            if (player == null)
+                throw new ArgumentNullException(nameof(player));
+            if (bank == null)
+                throw new ArgumentNullException(nameof(bank));
+            if (buy == offer)
+                throw new ArgumentException("Buy and Offer material cant be same type");
+            Player = player;
+            this.bank = bank;
+            this.buy = buy;
+            this.offer = offer;
+        }
+
+        public IPlayer Player { get; }
+
+        public GameCommand Type { get { return GameCommand.Trade; } }
+
+        public void Execute()
+        {
+            bank.BuyResource(buy, offer, Player);
+        }
+    }
+}
---
+++ test/VOC.Core.Test/Games/Commands/BuyResourceCommandTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using VOC.Core.Games.Commands;
+using VOC.Core.Items.RawMaterials;
+using VOC.Core.Players;
+using VOC.Core.Trading;
+using Xunit;
+
+namespace VOC.Core.Test.Games.Commands
+{
+    public class BuyResourceCommandTest
+    {
+        public static IEnumerable<object> NullConstruction
+        {
+            get
+            {
+                yield return new object[] { null, new Mock<IBank>().Object };
+                yield return new object[] { new Mock<IPlayer>().Object, null };
+            }
+        }
+
+        [Theory, MemberData(nameof(NullConstruction))]
+        public void CantConstructWithNullParameter(IPlayer player, IBank bank)
+        {
+            Assert.Throws<ArgumentNullException>(() => new BuyResourceCommand(player, bank, MaterialType.Grain, MaterialType.Wool));
+        }
+
+        [Fact]
+        public void ExpectExceptionIfOfferAndRequestAreOfSameType()
+        {
+            var player = new Mock<IPlayer>();
+            var bank = new Mock<IBank>();
+            Assert.Throws<ArgumentException>(() => new BuyResourceCommand(player.Object, bank.Object, MaterialType.Wool, MaterialType.Wool));
+        }
+
+        [Fact]
+        public void ExecuteCallsBank()
+        {
+            var player = new Mock<IPlayer>();
+            var bank = new Mock<IBank>();
+            var command = new BuyResourceCommand(player.Object, bank.Object, MaterialType.Wool, MaterialType.Lumber);
+            command.Execute();
+
+            bank.Verify(b => b.BuyResource(MaterialType.Wool, MaterialType.Lumber, player.Object));
+        }
+    }
+}
