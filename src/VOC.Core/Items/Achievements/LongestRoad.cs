861ea95bb8620d4c68236cc8935332e6a374f164
Corne van Beek <cornevbeek123@live.nl>
2016-03-31 21:43:32 +0200

    implemented LongestRoad

--- src/VOC.Core/Items/Achievements/LongestRoad.cs
+++ src/VOC.Core/Items/Achievements/LongestRoad.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VOC.Core.Boards;
+using VOC.Core.Players;
 
 namespace VOC.Core.Items.Achievements
 {
-    public class LongestRoad
+    /// <summary>
+    /// Longest Road will be given to first player with 5 connecting roads, not counting forks
+    /// If another player builds a longer road he will take the achievement
+    /// longest road is 2 victorypoints
+    /// </summary>
+    public class LongestRoad : IAchievement
     {
+        private readonly IBoard board;
+        public LongestRoad(IBoard board)
+        {
+            if (board == null)
+                throw new ArgumentNullException(nameof(board));
+            this.board = board;
+        }
+        private IPlayer _owner;
+        public IPlayer Owner
+        {
+            get { return _owner; }
+            set { _owner = value; OwnerChanged?.Invoke(this, Owner); }
+        }
+
+        public int VictoryPoints { get { return 2; } }
+
+        public event EventHandler<IPlayer> OwnerChanged;
+
+        public void Update(IPlayer player)
+        {
+            if (player == null)
+                throw new ArgumentNullException(nameof(player));
+
+            //Don't have to check if the current owner is equal to the player
+            if (Owner == player)
+                return;
+
+            var longestroadSize = board.GetLongestRoad(player).Count();
+            if (Owner == null && longestroadSize >= 5)
+                Owner = player;
+            else if (Owner != null && longestroadSize > board.GetLongestRoad(Owner).Count())
+                Owner = player;
+        }
     }
 }
---
+++ test/VOC.Core.Test/Items/Achievements/LongestRoadtest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using VOC.Core.Boards;
+using VOC.Core.Items.Achievements;
+using VOC.Core.Players;
+using Xunit;
+
+namespace VOC.Core.Test.Items.Achievements
+{
+    public class LongestRoadTest
+    {
+        [Fact]
+        public void CantConstructWithNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new LongestRoad(null));
+        }
+
+        [Fact]
+        public void CantUpdateWithNullPlayer()
+        {
+            var board = new Mock<IBoard>();
+            var achievement = new LongestRoad(board.Object);
+
+            Assert.Throws<ArgumentNullException>(() => achievement.Update(null));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(4)]
+        public void OwnerStaysNullIfRoadNotLongerThen4(int roadcount)
+        {
+            var roads = Enumerable.Range(0, roadcount).Select(i => new Mock<IRoad>().Object);
+            var player = new Mock<IPlayer>();
+            var board = new Mock<IBoard>();
+            board.Setup(b => b.GetLongestRoad(player.Object)).Returns(roads);
+
+            var achievement = new LongestRoad(board.Object);
+            bool changed = false;
+            achievement.OwnerChanged += (sender, args) => { changed = true; };
+
+            achievement.Update(player.Object);
+
+            Assert.False(changed);
+            Assert.Null(achievement.Owner);
+        }
+
+        [Fact]
+        public void PlayerTakesAchievementIfMinimumOf5()
+        {
+            var roads = Enumerable.Range(0, 5).Select(i => new Mock<IRoad>().Object);
+            var player = new Mock<IPlayer>();
+            var board = new Mock<IBoard>();
+            board.Setup(b => b.GetLongestRoad(player.Object)).Returns(roads);
+
+            var achievement = new LongestRoad(board.Object);
+            bool changed = false;
+            achievement.OwnerChanged += (sender, args) => { changed = true; };
+
+            achievement.Update(player.Object);
+
+            Assert.True(changed);
+            Assert.Equal(player.Object, achievement.Owner);
+        }
+
+        [Fact]
+        public void PlayerDoesNotOvertakeAchievementIfRoadOfSameSize()
+        {
+            var roads1 = Enumerable.Range(0, 5).Select(i => new Mock<IRoad>().Object);
+            var roads2 = Enumerable.Range(0, 5).Select(i => new Mock<IRoad>().Object);
+            var player1 = new Mock<IPlayer>();
+            var player2 = new Mock<IPlayer>();
+            var board = new Mock<IBoard>();
+            board.Setup(b => b.GetLongestRoad(player1.Object)).Returns(roads1);
+            board.Setup(b => b.GetLongestRoad(player2.Object)).Returns(roads2);
+
+            var achievement = new LongestRoad(board.Object);
+            achievement.Update(player1.Object);
+            achievement.Update(player2.Object);
+            Assert.Equal(player1.Object, achievement.Owner);
+        }
+
+        [Fact]
+        public void PlayerOvertakesAchievementIfonger()
+        {
+            var roads1 = Enumerable.Range(0, 5).Select(i => new Mock<IRoad>().Object);
+            var roads2 = Enumerable.Range(0, 6).Select(i => new Mock<IRoad>().Object);
+            var player1 = new Mock<IPlayer>();
+            var player2 = new Mock<IPlayer>();
+            var board = new Mock<IBoard>();
+            board.Setup(b => b.GetLongestRoad(player1.Object)).Returns(roads1);
+            board.Setup(b => b.GetLongestRoad(player2.Object)).Returns(roads2);
+
+            var achievement = new LongestRoad(board.Object);
+            achievement.Update(player1.Object);
+            achievement.Update(player2.Object);
+            Assert.Equal(player2.Object, achievement.Owner);
+        }
+    }
+}
