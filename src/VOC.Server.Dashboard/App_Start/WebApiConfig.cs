397306709d360534545d372d8ec7ed91836ec89a
Corne van Beek <cornevbeek123@live.nl>
2016-05-11 20:43:13 +0200

    switch .net core project back to default 4.6 project

---
+++ src/VOC.Server.Dashboard/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using VOC.Server.Dashboard.Models;
+
+namespace VOC.Server.Dashboard
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            //Dependency configuration
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<GameStore>().As<IGameStore>().SingleInstance();
+            builder.RegisterWebApiFilterProvider(config);
+
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
--- src/VOC.Server.Dashboard/Controllers/GamesController.cs
+++ src/VOC.Server.Dashboard/Controllers/GamesController.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
+using System.Web.Http;
 using VOC.Server.Dashboard.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace VOC.Server.Dashboard.Controllers
 {
-    [Route("api/[controller]")]
-    public class GamesController : Controller
+    public class GamesController : ApiController
     {
         private readonly IGameStore gamestore;
 
@@ -21,22 +20,18 @@ namespace VOC.Server.Dashboard.Controllers
         }
 
         // GET: api/values
-        [HttpGet]
         public IEnumerable<Game> Get()
         {
             return gamestore.Games;
         }
 
-        // POST api/values
-        [HttpPost]
+
         public void Post([FromBody]Game game)
         {
             //todo validation
             gamestore.Add(game);
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
         public void Delete(Guid id)
         {
             gamestore.Remove(id);
